1d4a2639f206f54f03a70e2a96af111c4302a59a
linguopeng <linguopeng@cryptape.com>
2022-08-08 13:34:23 +0800

    fix: add try sh

---
+++ try.sh
@@ -0,0 +1,16 @@
+if echo "$1" | grep -Eq '^[0-9]+'; then
+    try=$1
+    shift && cmd="$@"
+else
+    try=3
+    cmd="$@"
+fi
+for ((i=1; i<=${try};i++))
+do
+  $cmd
+  ret=$?
+  if [[ ${ret} == 0 ]];then
+    exit ${ret}
+  fi
+done
+exit ${ret}
