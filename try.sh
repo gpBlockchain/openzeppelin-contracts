8e12ead35d9a10d53cc1c0b0ebc85efc876a3795
linguopeng <linguopeng@cryptape.com>
2022-08-08 13:57:26 +0800

    fix: add try sh

--- try.sh
+++ try.sh
@@ -6,12 +6,13 @@ else
     try=3
     cmd="$@"
 fi
-for ((i=1; i<=${try};i++))
-do
+i=0; 
+while [[ "$i" -lt "${try}" ]]; do
   $cmd
   ret=$?
   if [[ ${ret} == 0 ]];then
     exit ${ret}
   fi
+  i=$((i + 1))
 done
 exit ${ret}
